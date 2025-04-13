2fe6693f27bb756d07d1cf11a1a9f965f593270d
WuDiXianXin <wudixianxin@163.com>
2025-04-13 22:41:33 +0000

    fix wlogout && maple_mono_nerd_font

--- archlinux_config/.config/nvim/lua/config/keymaps.lua
+++ archlinux_config/.config/nvim/lua/config/keymaps.lua
@@ -2,9 +2,19 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.api.nvim_set_keymap("i", "kj", "<Esc>", { noremap = true, silent = true, desc = "映射返回键" })
+vim.keymap.set("n", "<A-k>", ":m-2<CR>", { noremap = true, silent = true, desc = "Swap line up" })
+vim.keymap.set("n", "<A-j>", ":m+<CR>", { noremap = true, silent = true, desc = "Swap line down" })
+vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true, desc = "Move lines up" })
+vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true, desc = "Move lines down" })
 vim.api.nvim_set_keymap(
   "n",
-  "<leader>o",
-  ":!xdg-open %<CR>",
-  { noremap = true, silent = true, desc = "用系统程序打开当前文件" }
+  "<leader>bc",
+  ":bufdo bwipeout<cr>",
+  { noremap = true, silent = true, desc = "彻底删除缓冲区" }
+)
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>mdp",
+  ":MarkdownPreviewToggle<cr>",
+  { noremap = true, silent = true, desc = "映射返回键" }
 )
--- archlinux_config/.config/nvim/lua/config/lazy.lua
+++ archlinux_config/.config/nvim/lua/config/lazy.lua
@@ -16,37 +16,36 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   spec = {
-    -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- import/override with your plugins
     { import = "plugins" },
   },
   defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-    lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
-    version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
+    lazy = true,
+    version = "*",
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
   checker = {
-    enabled = true, -- check for plugin updates periodically
-    notify = false, -- notify on update
-  }, -- automatically check for plugin updates
+    enabled = true,
+    notify = true,
+  },
   performance = {
     rtp = {
       -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
+        -- "matchit", -- 增强 % 键的匹配能力
+        -- "matchparen", -- 高亮匹配的括号/引号
+        "netrwPlugin", -- 内置文件浏览器
         "tarPlugin",
-        "tohtml",
-        "tutor",
+        "tohtml", -- 将代码转换为 HTML
+        "tutor", -- Neovim 内置教程
         "zipPlugin",
+        "2match",
+        "getscriptPlugin",
+        "manPlugin", -- 内置Man命令
+        "vimballPlugin",
+        "logiPat", -- 逻辑模式匹配
+        -- "spellfile", -- 拼写检查字典管理
       },
     },
   },
